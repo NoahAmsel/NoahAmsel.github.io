4fdc507ebcf69658645d912c9c5b234e725ee3be
Noah Amsel <noah.amsel@nyu.edu>
2022-08-31 03:35:58 -0400

    talks from json

--- markdown_generator/generate_talks.py
+++ markdown_generator/generate_talks.py
@@ -1,29 +1,30 @@
 import datetime
+import json
 import os.path
 import sys
 
 from inspect import cleandoc
-from pandas import read_csv
 
 from utils import *
 
-talks_table = read_csv(sys.argv[1], sep="\t", header=0, quotechar='`')
+with open(sys.argv[1]) as f:
+    talks_data = json.load(f)
 
-for row, item in talks_table.iterrows():
-    date = datetime.datetime.strptime(item.date, "%Y-%m-%d").date()
-    url_slug = '-'.join((date.isoformat(), *urlify(item.title).split('-')[:5]))
-    extras = {name: item[name] for name in ('pdf_url', 'video_url', 'slides_url', 'code_url') if name in item}
+for talks_data in talks_data:
+    date = datetime.datetime.strptime(talks_data["date"], "%Y-%m-%d").date()
+    url_slug = '-'.join((date.isoformat(), *urlify(talks_data["title"]).split('-')[:5]))
+    extras = {name: talks_data[name] for name in ('pdf_url', 'video_url', 'slides_url', 'code_url') if name in talks_data}
 
     with open(os.path.join(sys.argv[2], f"{url_slug}.md"), 'w') as f:
         f.write(markdown_page(
-            item.description,
-            title=item.title,
+            talks_data["description"],
+            title=talks_data["title"],
             collection="talks",
-            type=item.type,
+            type=talks_data["type"],
             permalink=f"/talks/{url_slug}",
-            venue=item.venue,
+            venue=talks_data["venue"],
             date=date.isoformat(),
-            location=item.location,
-            published=item.published,
+            location=talks_data["location"],
+            published=talks_data["published"],
             **extras
         ))
